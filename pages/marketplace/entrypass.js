674cca6abced947c431db45a316af5dbe5a550c4
“satyam141026cs” <satyam@sdlccorp.com>
2023-05-18 17:01:25 +0530

    add entry code

---
+++ pages/marketplace/entrypass.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Line from "../../src/images/monthpassline.png";
+/* import Garena from "../../src/images/avatars/garena.png";
+import anyone from "../../src/images/avatars/anyone.png";
+import Image from "next/image";*/
+import Entrypassformet from "pages/shared/entrypassformet"; 
+
+
+const entrypass = () => {
+    let data=[
+        {
+            smallbgimgclass:"bgMonthPassSmall",
+            smallimg:"Garena",
+            bigimgclass:"bgMonthPass",
+            month:"1"
+        },
+        {
+            smallbgimgclass:"bgMonthPassSmalltwo",
+            smallimg:"anyone",
+            bigimgclass:"bgMonthPasstwo",
+            month:"2"
+        },
+        
+    ]
+  return (
+    <div className="flex flex-col gap-16">
+     
+
+      {
+       data.length && data.map((item,index)=>{
+            return (
+             <Entrypassformet data={item}/>
+            )
+        })
+      }
+    </div>
+  )
+}
+
+export default entrypass

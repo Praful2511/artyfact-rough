163d4282b64f0c787f9527d3f3f8344bc90b2e13
“satyam141026cs” <satyam@sdlccorp.com>
2023-05-18 18:39:51 +0530

    Fix code

--- pages/marketplace/entrypass.js
+++ pages/marketplace/entrypass.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Line from "../../src/images/monthpassline.png";
-/* import Garena from "../../src/images/avatars/garena.png";
+ import Garena from "../../src/images/avatars/garena.png";
 import anyone from "../../src/images/avatars/anyone.png";
-import Image from "next/image";*/
+import Image from "next/image";
 import Entrypassformet from "pages/shared/entrypassformet"; 
 
 
@@ -10,13 +10,13 @@ const entrypass = () => {
     let data=[
         {
             smallbgimgclass:"bgMonthPassSmall",
-            smallimg:"Garena",
+            smallimg:Garena,
             bigimgclass:"bgMonthPass",
             month:"1"
         },
         {
             smallbgimgclass:"bgMonthPassSmalltwo",
-            smallimg:"anyone",
+            smallimg:anyone,
             bigimgclass:"bgMonthPasstwo",
             month:"2"
         },
@@ -26,13 +26,13 @@ const entrypass = () => {
     <div className="flex flex-col gap-16">
      
 
-      {
+   {
        data.length && data.map((item,index)=>{
             return (
              <Entrypassformet data={item}/>
             )
         })
-      }
+      } 
     </div>
   )
 }
--- pages/shared/entrypassformet.js
+++ pages/shared/entrypassformet.js
@@ -1,6 +1,6 @@
 import React from "react";
-/* import Line from "../../public/images/monthpassline.png"; */
-
+ import Line from "../../src/images/monthpassline.png"; 
+import logo from "../../src/images/logo_small.png"
 import Image from "next/image";
 const Entrypassformet = ({ data}) => {
 	
@@ -8,8 +8,7 @@ const Entrypassformet = ({ data}) => {
     <div className="flex gap-7 lg:gap-2 2xl:gap-3  flex-wrap ">
       <div className={`w-[320px]  xl:w-[243px] 2xl:w-[320px] h-[286px]  ${data.smallbgimgclass}`}>
         <div className="flex h-[250px] items-center justify-center">
-	
-         {/*  <Image className="  mt-[78px]" alt="line" src={data?.smallimg.src} width={data?.smallimg.width} height={data?.smallimg.height} /> */}
+        <Image className="  mt-[78px]" alt="line" src={data?.smallimg.src} width={data?.smallimg.width} height={data?.smallimg.height} />
         </div>
       </div>
       <div className={`w-full lg:w-[786px] h-[286px]  mb-7 lg:mb-0 ${data.bigimgclass} `}>
@@ -18,7 +17,7 @@ const Entrypassformet = ({ data}) => {
             {data?.month} MONTH PASS
           </h1>
           <p className="my-2">Play game & win price</p>
-         {/*  <Image className="h-max my-8" alt="line" src={Line} /> */}
+          <Image className="h-max my-8" alt="line" src={Line} />
           <p className="text-[#595959] text-sm my-2">Price</p>
           <div className="flex justify-between">
             <div className="flex gap-4">
@@ -35,7 +34,7 @@ const Entrypassformet = ({ data}) => {
                 <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">=</sub>
               </div>
               <div className="flex gap-2 items-center">
-              {/*   <img src="../logo_small.png" alt="logo" /> */}
+              <Image src={logo} alt="logo" />
                 <p className="text-lg">
                   121
                   <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">

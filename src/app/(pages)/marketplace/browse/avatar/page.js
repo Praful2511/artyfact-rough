bdf2b707fef6ef2dee7abf8cdf1ff06225d17a97
“satyam141026cs” <satyam@sdlccorp.com>
2023-05-19 11:52:16 +0530

    fix weapon,avtar and backpack page card

--- src/app/(pages)/marketplace/browse/avatar/page.js
+++ src/app/(pages)/marketplace/browse/avatar/page.js
@@ -1,19 +1,143 @@
+import AvatarCard from "@/components/common/avatarCard";
+import avt1 from "@/images/avatars/avatar1.png";
+import avt2 from "@/images/avatars/avatar2.png";
+import avt3 from "@/images/avatars/avatar3.png";
+import avt4 from "@/images/avatars/avatar4.png";
+import avt5 from "@/images/avatars/avatar5.png";
+import avt6 from "@/images/avatars/avatar6.png";
+import avt7 from "@/images/avatars/avatar7.png";
+import avt8 from "@/images/avatars/avatar8.png";
+import avt9 from "@/images/avatars/avatar9.png";
+import avt10 from "@/images/avatars/avatar10.png";
+import avt11 from "@/images/avatars/avatar11.png";
+import avt12 from "@/images/avatars/avatar12.png";
+import avt13 from "@/images/avatars/avatar13.png";
+
 export default function AvatarPage() {
-    return (<main>
+  let avatars = [
+    {
+      img: avt1,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt2,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt3,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt4,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt5,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt6,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt7,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt8,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt9,
+      name: "Vitalik Buterin",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt10,
+      name: "Vitalik Buterin",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt11,
+      name: "Vitalik Buterin",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt12,
+      name: "Vitalik Buterin",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: avt13,
+      name: "Vitalik Buterin",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+  ];
+  return (
+    <main>
+   {/*    <div>
+        <div>Avatar</div>
         <div>
-            <div>Avatar</div>
+          <div>seacrch</div>
+          <div>
             <div>
-                <div>seacrch</div>
-                <div>
-                    <div> <span>Sort By :</span>
-                    </div>
-                    <div>
-                        
-                    </div>
-                </div>
+              {" "}
+              <span>Sort By :</span>
             </div>
+            <div></div>
+          </div>
         </div>
-        <div>cards</div>
-        <div>pagination</div>
-    </main>)
+      </div> */}
+      <div className="mb-20">
+        <div className="mx-6 mt-8">
+          <div className="flex justify-between">
+            <div className="font-normal text-xl">AVATAR</div>
+            <div>See All</div>
+          </div>
+          <div className="w-full flex flex-wrap gap-9 mt-6">
+            {avatars.length
+              ? avatars.map((item, index) => (
+                  <AvatarCard key={index} data={item} />
+                ))
+              : ""}
+          </div>
+        </div>
+      </div>
+      <div>pagination</div>
+    </main>
+  );
 }
--- src/app/(pages)/marketplace/browse/weapon/page.js
+++ src/app/(pages)/marketplace/browse/weapon/page.js
@@ -1,3 +1,100 @@
+import gun1 from "@/images/weapons/gun1.png";
+import gun2 from "@/images/weapons/gun2.png";
+import gun3 from "@/images/weapons/gun2.png";
+import gun4 from "@/images/weapons/gun2.png";
+import gun5 from "@/images/weapons/gun2.png";
+import gun6 from "@/images/weapons/gun2.png";
+import gun7 from "@/images/weapons/gun2.png";
+import gun8 from "@/images/weapons/gun2.png";
+import gun9 from "@/images/weapons/gun2.png";
+import gun10 from "@/images/weapons/gun2.png";
+import SmallCard from "@/components/common/smallCard";
 export default function WeaponPage() {
-    return <h1>WeaponPage</h1>
+  let weapons = [
+    {
+      img: gun1,
+      name: "Shot Gun",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun2,
+      name: "Shot Gun",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun3,
+      name: "Shot Gun",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun4,
+      name: "Shot Gun",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun5,
+      name: "Shot Gun",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun6,
+      name: "Shot Gun",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun7,
+      name: "Shot Gun",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun8,
+      name: "Shot Gun",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun9,
+      name: "Shot Gun",
+      price: "30",
+      level: "GOLD",
+      value: 25,
+    },
+    {
+      img: gun10,
+      name: "Shot Gun",
+      price: "20",
+      level: "GOLD",
+      value: 25,
+    },
+  ];
+  return (
+    <div className="mb-20">
+    <div className='mx-6 mt-8'>
+			<div className='flex justify-between'>
+				<div className='font-normal text-xl'>WEAPON SKIN</div>
+				<div>See All</div>
+			</div>
+			<div className="w-full flex flex-wrap gap-9 mt-6">
+				{weapons.length
+					? weapons.map((item, index) => <SmallCard key={index} data={item}/>)
+					: ""}
+			</div>
+		</div>
+    </div>
+  );
 }

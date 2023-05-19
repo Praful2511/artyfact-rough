97aef9e89b2309aa980caebdc7dfb27378ba9685
“satyam141026cs” <satyam@sdlccorp.com>
2023-05-19 13:01:14 +0530

    fix line

--- src/app/(pages)/entry-pass/entrypass.js
+++ src/app/(pages)/entry-pass/entrypass.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Line from "../../../images/monthpassline.png";
+import Line from "../../../images/monthpassline1.png";
 import Garena from "../../../images/avatars/garena.png";
 import anyone from "../../../images/avatars/anyone.png";
 import Image from "next/image";
@@ -42,44 +42,49 @@ const Entrypass = () => {
         <div
           className={`w-full lg:w-[784px] h-[285px]  mb-7 lg:mb-0 bgMonthPass `}
         >
-          <div className="!p-4 mt-3">
-            <h1 className="text-white !text-xl Starzone">1 MONTH PASS</h1>
-            <p className="my-2">Play game & win price</p>
-            <Image className="h-max my-8" alt="line" src={Line} />
-            <p className="text-[#595959] text-sm my-2">Price</p>
-            <div className="flex justify-between">
-              <div className="flex gap-4">
-                <div>
-                  {" "}
-                  <p className="text-lg">
-                    10
+          <div className=" mt-3">
+            <div className="px-9 pt-7">
+              <h1 className="text-white !text-xl Starzone">1 MONTH PASS</h1>
+              <p className="my-2">Play game & win price</p>
+            </div>
+
+            <Image className="h-max my-4" alt="line" src={Line} />
+            <div className="px-9  ">
+              <p className="text-[#595959] text-sm my-2">Price</p>
+              <div className="flex justify-between">
+                <div className="flex gap-4">
+                  <div>
+                    {" "}
+                    <p className="text-lg">
+                      10
+                      <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
+                        USD
+                      </sub>
+                    </p>
+                  </div>
+                  <div className="text-lg">
                     <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
-                      USD
+                      =
                     </sub>
-                  </p>
+                  </div>
+                  <div className="flex gap-2 items-center -mt-3">
+                    <Image src={logo} alt="logo" />
+                    <p className="text-lg">
+                      121
+                      <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
+                        $ARTY
+                      </sub>
+                    </p>
+                  </div>
                 </div>
-                <div className="text-lg">
-                  <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
-                    =
-                  </sub>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <Image src={logo} alt="logo" />
-                  <p className="text-lg">
-                    121
-                    <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
-                      $ARTY
-                    </sub>
-                  </p>
-                </div>
-              </div>
-              <div>
-                {/*  <button className="border border-[#F8A932] rounded-xl lg:w-[188px] h-[41px] text-center px-3">
+                <div className="mr-20">
+                  {/*  <button className="border border-[#F8A932] rounded-xl lg:w-[188px] h-[41px] text-center px-3">
                   GET A PASS
                 </button> */}
-                <InvertButton className={"!lg:w-[188px]"}>
-                  GET A PASS
-                </InvertButton>
+                  <InvertButton className={"!lg:w-[188px]"}>
+                    GET A PASS
+                  </InvertButton>
+                </div>
               </div>
             </div>
           </div>
@@ -96,46 +101,55 @@ const Entrypass = () => {
         <div
           className={`w-full lg:w-[784px] h-[285px]  mb-7 lg:mb-0 bgMonthPasstwo `}
         >
-          <div className="!p-4 mt-3">
-            <h1 className="text-white !text-xl Starzone">1 MONTH PASS</h1>
-            <p className="my-2">Play game & win price</p>
-            <Image className="h-max my-8" alt="line" src={Line} />
-            <p className="text-[#595959] text-sm my-2">Price</p>
-            <div className="flex justify-between">
-              <div className="flex gap-4">
-                <div>
-                  {" "}
-                  <p className="text-lg">
-                    10
+          <div className=" mt-3">
+            <div className="px-9 pt-7">
+              <h1 className="text-white !text-xl Starzone">2 MONTH PASS</h1>
+              <p className="my-2">Play game & win price</p>
+            </div>
+
+            <Image className="h-max my-4" alt="line" src={Line} />
+            <div className="px-9  ">
+              <p className="text-[#595959] text-sm my-2">Price</p>
+              <div className="flex justify-between">
+                <div className="flex gap-4">
+                  <div>
+                    {" "}
+                    <p className="text-lg">
+                      10
+                      <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
+                        USD
+                      </sub>
+                    </p>
+                  </div>
+                  <div className="text-lg">
                     <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
-                      USD
+                      =
                     </sub>
-                  </p>
+                  </div>
+                  <div className="flex gap-2 items-center -mt-3">
+                    <Image src={logo} alt="logo" />
+                    <p className="text-lg">
+                      121
+                      <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
+                        $ARTY
+                      </sub>
+                    </p>
+                  </div>
                 </div>
-                <div className="text-lg">
-                  <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
-                    =
-                  </sub>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <Image src={logo} alt="logo" />
-                  <p className="text-lg">
-                    121
-                    <sub className="ml-1 text-[9px] Starzone text-[#BCBCBD]">
-                      $ARTY
-                    </sub>
-                  </p>
-                </div>
-              </div>
-              <div>
-                <InvertButton className={"!lg:w-[188px]"}>
+                <div className="mr-20">
+                  {/*  <button className="border border-[#F8A932] rounded-xl lg:w-[188px] h-[41px] text-center px-3">
                   GET A PASS
-                </InvertButton>
+                </button> */}
+                  <InvertButton className={"!lg:w-[188px]"}>
+                    GET A PASS
+                  </InvertButton>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+     
       <div className="mb-10"></div>
     </div>
   );
